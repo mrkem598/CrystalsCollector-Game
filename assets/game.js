fda6cd2375ebd0c8c346a62e58e7c0efd9115fac
Moh <mohakemal9@gmail.com>
2017-03-06 03:49:42 -0500

    3rd commit!

--- assets/game.js
+++ assets/game.js
@@ -1,71 +1,70 @@
-$(document).ready(function() {
-var crystals = [
-	'assets/images/red.png',
-	'assets/images/blue.png',
-	'assets/images/yellow.png',
-	'assets/images/green.png'
-	];
-
+// Gobal variables that are accesible for all
 var wins = 0;
 var losses = 0;
-var score = 0;
-
-newCrystals();
-newGame();
-	//creating a function for newCrystals
-	function newCrystals () {
-		var numbers = []
-		while(numbers.length < 4) {
-		//Math.ceil round a number upward to its nearest integer
-			var winningNumber = Math.ceil(Math.random()* 12);
-			var found = false;
-			for (var i=0; i< numbers.length ; i++){
-				if(numbers[i] === winningNumber) {
-					found = true; break
-				}
-			}
-			if(!found)numbers[numbers.length]=winningNumber;
-		}
-		console.log(numbers);
-	for(i=0; i < numbers.length; i++) {
-	 var imageCrystal = $('<img>');
-	 //The attr() method sets or returns attributes and values of the imageCrystal
-	imageCrystal.attr('data-num', numbers[i]);
-	imageCrystal.attr('src', crystals[i]);
-	imageCrystal.attr('alt', 'crystals');
-	imageCrystal.addClass('crystalImage');
-	$("#crystals").append('imageCrystal');
-	}
-	}
-function newGame() {
-	var score = 0;
-	$("#yourScore").text(score);
-	
-	function randomIntFromInterval(min,max) {
-		return Math.floor(Math.random()*(max-min)+1);
-	}
-	var numberToGuess = randomIntFromInterval(19,120);
-	$(".value").text(numberToGuess);
-	
-	$(".crystalImage").on("click", function() {
-		counter = counter + parseInt($(this).data("num"));
-		$("#yourScore").text((counter);
-		if(counter === numberToGuess) {
-			$("#status").text("You Win!");
-			wins ++:
-			$("#win").text(wins);
-			console.log(wins);
-			$("#crystals").empty();
-			newCrystals();
-			newGame();
-		}else if (score > numberToGuess){
-			$("#status).text("You Lost!");
-			  losses ++;
-			  console.log(losses);
-			  $("#crystals").empty();
-			newCrystals();
-			newGame();
-		}
-		});
+//Players current score 
+var addition = 0;
+// selecting a rundom number
+var total = Math.floor(Math.random()* 100) + 1);
+$(".total").empty();
+$(".total".append(total);
+//assigns a random number to each crystals 
+var red = Math.floor(Math.random()* 10) + 1);
+var blue = Math.floor(Math.random()* 10) +1);
+var yellow = Math.floor(Math.random()* 10) +1);
+var green = Math.floor(Math.random)* 10) +1);
+//appending the addition in the HTML
+var updateAddition = fucntion (){
+	$(".addition").empty();
+	$(".addition").append(addition);
+	$("#wins").empty();
+	$("#wins").append(wins);
+	$("#losses").empty();
+	$("#losses").append(losses);
 }
+//Restarting the function can be achieved as follow
+var restart = function(){
+	addition = 0;
+	total = Math.floor(Math.random()* 100)+ 1);
+	$(".total").empty();
+	$(".total").append(total);
+	red = Math.floor(Math.random()* 10) + 1);
+	blue = Math.floor(Math.random()*10)+ 1);
+	yellow = Math.floor(Math.random()*10) +1);
+	green = Math.floor(Math.random()* 10) +1);
+	updateAddition();
+}
+//the logic to determin win and losses
+var logic = function(){
+	if(addition == total) {
+		wins = wins + 1;
+		alert("Bravo You Win!");
+		restart();
+	}else if(addition > total){
+		losses = losses + 1;
+		alert("You Lost!");
+		restart();
+	}else{
+		updateAddition();
+	}
+}
+$(".addition").append(total);
+$(".addition").append(addition);
+//The on click function
+$(document).ready(function(){
+	$("#red").click(function(){
+		addition = addition + red;
+		logic();
+	})
+$("#blue").click(function(){
+	addition = addition + blue;
+	logic();
+})
+$("#yellow").click(function(){
+	addition = addition + yellow;
+	logic();
+})
+$("#green").click(function(){
+	addition = addition + green;
+	logic();
+})
 });
